427523d62f59178bb343bf1f47de9c963287a709
proin225 <53169953+proin225@users.noreply.github.com>
2020-05-09 21:39:04 +0900

    V 1.0.0
     - 레코드 수정 명령어에 "-s" 옵션 추가
     - IP표시 문구 수정
     - 정식 릴리즈

--- cloudflare_ddns.sh
+++ cloudflare_ddns.sh
@@ -3,10 +3,12 @@ Global_Api_Key=1234567890123456789012345678901234567
 Domain=example.com
 A_Record=a.example.com
 
+echo "========== Cloudflare DDNS SCRIPT V 1.0.0 =========="
+
 IP=$(curl -s "https://ipv4.icanhazip.com/")
 API="https://api.cloudflare.com/client/v4"
 
-echo $IP
+echo "Ipv4 : " $IP
 
 H_Email="-HX-Auth-Email:$Email"
 H_Auth_Key="-HX-Auth-Key:$Global_Api_Key"
@@ -22,7 +24,7 @@ Record_ID=$(curl -s -X GET "$API/zones/$Zone_ID/dns_records?type=A&name=$A_Recor
 	$H_Email $H_Auth_Key $H_Content \
 	| grep '"id"' | sed -e s/'"id": "'//g | sed -e s/'",'//g | sed s/' '//g)
 
-PUT=$(curl -X PUT "$API/zones/$Zone_ID/dns_records/$Record_ID" \
+PUT=$(curl -s -X PUT "$API/zones/$Zone_ID/dns_records/$Record_ID" \
 	$H_Email $H_Auth_Key $H_Content \
 	--data '{"type":"A","name":"'"$A_Record"'","content":"'"$IP"'"}')
 
